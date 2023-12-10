5ebc8071c506a0119151a196f8b4de16b5c22ab3
lagom-QB <pacheconlucky@gmail.com>
2023-12-10 09:24:37 +0100

    ft. Debugging

--- app_scripts/app.py
+++ app_scripts/app.py
@@ -65,7 +65,7 @@ if st.button('Analyze'):
     st.markdown(f'Time taken to analyze: `{end - start}`')
 
     # Print the data rows with the same common_topics as the inputText
-    st.write(data[data.common_topics == data.loc[data.sentiment == inputText, 'common_topics'].values[0]][['sentiment','huggingFace_autoTokenizer','huggingFace_Distilbert', 'common_topics']])
+    st.write(data[data.common_topics == data.loc[data.sentiment == inputText, 'common_topics'].values[0]])
 
 st.markdown(
     '`Created by` [Brenda](https://github.com/lagom-QB) | \
