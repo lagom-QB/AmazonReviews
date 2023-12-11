166ac1a2ba4035407cdacc39a8e2439205305ec9
lagom-QB <pacheconlucky@gmail.com>
2023-12-11 09:37:26 +0100

    ft. Debugging

--- app_scripts/load_preprocess_data.py
+++ app_scripts/load_preprocess_data.py
@@ -18,8 +18,11 @@ def load_data(file_loc='assets/data.csv', inputText=None, inputRating=None):
         st.write(f'File not found at {file_loc}')
         raise FileNotFoundError(f'File not found at {file_loc}')
     else:
-        st.write(f'File not found at {file_loc} ... Folder contains: {os.listdir("assets")}')
-        data = pd.read_csv(file_loc, names=[f'column_{i}' for i in range(0, 6)])
+        # st.write(f'File not found at {file_loc} ... Folder contains: {os.listdir("assets")}') # data.csv
+        data = pd.read_csv(file_loc)
+        num_cols = len(data.columns)
+        data.columns = [f'column_{i}' for i in range(0, num_cols+1)]
+        
         st.write(f'Data exists',data.head(10))
 
         if inputText is not None and inputRating is not None:
